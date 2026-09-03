288ef110c3f5711d4d8208f67cfb0a82b64336de
MIATSUNO <vinattore@gmail.com>
2026-09-03 00:10:03 -0300

    Kaystant: profile validation updates

--- kayepad-platform/kaystant-api/feature_endpoints.py
+++ kayepad-platform/kaystant-api/feature_endpoints.py
@@ -1,4 +1,4 @@
-from app import SQLModel, DBField, UUID, uuid4, datetime, UTC, BaseModel, Field, app, Session, engine, select, KUser, KPost, post_json, user_json, me, Depends, HTTPException
+from app import SQLModel, DBField, UUID, uuid4, datetime, UTC, BaseModel, Field, app, Session, engine, select, KUser, KPost, KSession, KInk, KPurchase, KGroup, KGroupMember, KPet, KBook, KBookPost, KPetTouch, post_json, user_json, me, Depends, HTTPException
 
 # Social graph and editable profiles
 class KFollow(SQLModel, table=True):
